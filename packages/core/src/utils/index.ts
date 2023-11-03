1cc0777de68a43f4863fcb266a719852a110a9bd
Paul <dev@suaveplan.com>
2023-11-03 08:31:55 +0000

    Updated indexes

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,13 +1,19 @@
 import {serialize, deserialize} from './serialize-deserialize'
 import {Service} from "typedi";
 import {AbstractClass} from "../service";
-
-
+import {getValidTlds,hasValidTld, splitDomain} from "./domain";
+import {normalizeEmail} from './email';
+import proxyWithFeatures from "./proxy";
 @Service('utils')
 class Utils extends AbstractClass {
 
-    public serializer = serialize;
-    public deserializer = deserialize;
+    serializer = serialize;
+    deserializer = deserialize;
+	getValidTlds = getValidTlds;
+	hasValidTld = hasValidTld;
+	splitDomain=splitDomain;
+	normalizeEmail=normalizeEmail;
+	proxyWithFeatures=proxyWithFeatures;
 
 }
 
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * as Server from '../packages/server/src'
+export * as Client from '../packages/client/src'
+export * as Core from '../packages/core/src'
